8819732307602f31478e4ab3a9dbda65d347a0c2
Mark Brooks <mark@streamsets.com>
2024-05-26 11:14:07 -0700

    updates

---
+++ README.md
@@ -0,0 +1,83 @@
+## StreamSets Hob Metrics to Snowflake
+This project provides an example of how to replicate StreamSets Job Metrics to Snowflake.  
+
+This allows operators to easily run SQL queries and build custom dashboards on top of StreamSets Job metrics and history. For example, one could display a list of currently running Jobs with their record counts, or compare historical record counts and performance metrics of the same Job run every day for the past month.
+
+*Important note: This example uses a [Shell Executor](https://docs.streamsets.com/portal/platform-datacollector/latest/datacollector/UserGuide/Executors/Shell.html#concept_jsr_zpw_tz).  For production use, make sure to configure [Data Collector Shell Impersonation Mode](https://docs.streamsets.com/portal/platform-datacollector/latest/datacollector/UserGuide/Executors/Shell.html#concept_n2w_txv_vz).*
+
+### Design Approach
+There are several ways one could implement this functionality:
+
+- A StreamSets pipeline could use the StreamSets REST API to get Job history and metrics and merge the data into a Snowflake table. The advantages of this approach include the use of a single pipeline without needing a Shell Executor, a Snowflake connector with merge and data-drift support, scheduling, monitoring and failover; however, getting and filtering Job history and metrics using the REST API is somewhat complex.
+
+- A [StreamSets SDK for Python](https://docs.streamsets.com/platform-sdk/latest/welcome/overview.html) script could perform the same actions. The advantages of this approach include an elegant and convenient syntax for getting Job history and metrics, but would require custom code to merge the data into Snowflake, and has no built in scheduling, monitoring and failover.
+
+- A hybrid approach! This example combines the use of a StreamSets SDK script with a pipeline, to get the best of both worlds: an elegant retrieval of Job history and metrics, and all of the operational benefits of using a StreamSets pipeline including scheduling, monitoring and failover. 
+
+The example pipeline allows the user to set a <code>lookback</code> period that determines how far back in time to get Job history and metrics for, for example 5 minutes, 1 hour, 24 hours, 30 days, etc...  
+
+The script is idempotent, so that running it multiple times will not result in duplicate data in Snowflake. For Jobs that are actively running across two or more executions of the script, the Job run's metrics will be updated.
+
+A Job could be created for this pipeline, and that Job could be scheduled to run as often as necessary, for example, to keep a dashboard updated every five minutes.
+
+
+
+### Prerequisites
+
+- A Python 3.6+ environment with the StreamSets Platform SDK v6.0+ module installed. This example was tested using Python 3.11.5 and StreamSets SDK v6.3.
+
+- StreamSets [API Credentials](https://docs.streamsets.com/portal/platform-controlhub/controlhub/UserGuide/OrganizationSecurity/APICredentials_title.html#concept_vpm_p32_qqb)
+
+## Deploying the Example
+
+### Test the SDK Script
+- Copy the file [get_streamsets_job_metrics.py](python/get_streamsets_job_metrics.py) to a location on the Data Collector machine. I'll copy mine to  <code>/home/mark/scripts/get_streamsets_job_metrics.py</code>
+
+- Export your StreamSets Platform API Credentials:
+```
+	$ export CRED_ID="..."
+	$ export CRED_TOKEN="..."
+```
+
+- Execute the script passing it the name of a metrics file to be written and the number of lookback minutes:
+
+```
+	$ python3 get_streamsets_job_metrics.py /tmp/streamsets_job_metrics.json 60
+```
+
+You should see output like this:
+
+```
+	$ python3 get_streamsets_job_metrics.py /tmp/streamsets_job_metrics.json 60
+	-------------------------------------
+	Current time is 2024-05-26 17:49:23
+	Lookback minutes is 60
+	Will get metrics for Jobs started after 2024-05-26 16:49:23
+	Metrics will be written to the file /tmp/streamsets_job_metrics.json
+	-------------------------------------
+	Connected to Control Hub
+	-------------------------------------
+	Found 7 Job Runs within lookback window
+	Writing Metrics
+	-------------------------------------
+	Done
+```
+
+Inspect the metrics file written by the script. It should look like this:
+
+```
+	$ cat /tmp/streamsets_job_metrics.json
+	{"ID": "208e479f-c34c-4379-8529-c03d5c6d3f60:8030c2e9-1a39-11ec-a5fe-97c8d4369386", "NAME": "Get Weather Events", "CREATETIME": 1696984398077, "LASTMODIFIEDON": 1716475664195, "PIPELINENAME": "Get Weather Events", "PIPELINECOMMITLABEL": "v39", "RUNCOUNT": 110, "STARTTIME": 1716745144841, "FINISHTIME": 0, "ERRORMESSAGE": null, "COLOR": "GREEN", "STATUS": "ACTIVE", "INPUTRECORDS": 499, "OUTPUTRECORDS": 499, "ERRORRECORDS": 0}
+	{"ID": "fe9605ab-4912-4181-a315-e49d031a0d50:8030c2e9-1a39-11ec-a5fe-97c8d4369386", "NAME": "Oracle to Snowflake Bulk Load", "CREATETIME": 1716561746294, "LASTMODIFIEDON": 1716562027770, "PIPELINENAME": "Oracle to Snowflake Bulk Load", "PIPELINECOMMITLABEL": "v9", "RUNCOUNT": 12, "STARTTIME": 1716745294569, "FINISHTIME": 1716745325738, "ERRORMESSAGE": null, "COLOR": "GRAY", "STATUS": "INACTIVE", "INPUTRECORDS": 26079, "OUTPUTRECORDS": 26082, "ERRORRECORDS": 0}
+	{"ID": "de4a50a5-7f81-4f55-8dd7-1fc8614c2148:8030c2e9-1a39-11ec-a5fe-97c8d4369386", "NAME": "Weather Raw to Refined", "CREATETIME": 1716330812999, "LASTMODIFIEDON": 1716475898652, "PIPELINENAME": "Weather Raw to Refined", "PIPELINECOMMITLABEL": "v26", "RUNCOUNT": 7, "STARTTIME": 1716745145236, "FINISHTIME": 0, "ERRORMESSAGE": null, "COLOR": "GREEN", "STATUS": "ACTIVE", "INPUTRECORDS": 497, "OUTPUTRECORDS": 497, "ERRORRECORDS": 0}
+	...
+```
+
+--> If you have made it this far. the SDK script is working!
+
+### Import and Configure the pipeline
+Import the pipeline from the archive file [pipelines/Job_History_to_Snowflake.zip](pipelines/Job_History_to_Snowflake.zip)
+
+The pipeline looks like this:
+<img src="images/pipeline.png" alt="pipeline" width="700"/>
+
---
+++ python/get_streamsets_job_metrics.py
@@ -0,0 +1,191 @@
+"""
+get_streamsets_job_metrics.py
+
+This script retrieves StreamSets Job history and metrics from StreamSets Platform and
+writes the results to a local file.  The script can be run standalone from the command line
+or can be called by a StreamSets pipeline's start-event, with the pipeline then merging the
+retrieved metrics into a Snowflake table.  See the README.md for details.
+
+Prerequisites
+-------------
+
+- Python 3.6+ (tested with Python 3.11.5)
+
+- StreamSets API Credentials set in the environment prior to running the script.
+  For example:
+    $ export CRED_ID="zzzz"
+    $ export CRED_TOKEN="zzzz"
+
+- The StreamSets SDK for Python v6.0+ (tested with v6.3)
+
+
+Command Line Arguments
+----------------------
+The script requires two command line arguments:
+
+- job_metrics_file - an absolute path to a file to write the metrics to. If the file
+  already exists, for example, from a prior run, the file will be overwritten.
+
+- lookback_minutes - How many minutes prior to the current time the script should retrieve
+  Job metrics for.
+
+
+Example Usage
+-------------
+$ python3 get_streamsets_job_metrics.py /tmp/streamsets_job_metrics.json 60
+
+"""
+
+import os
+from datetime import datetime
+from time import time
+import sys
+from streamsets.sdk import ControlHub
+import json
+
+# Get Control Hub Credentials from the environment
+cred_id = os.getenv('CRED_ID')
+cred_token = os.getenv('CRED_TOKEN')
+
+# The file we'll write metrics to
+output_file = None
+
+
+def print_usage_and_exit():
+    print('Usage: $ python3 get_streamsets_job_metrics.py <job_metrics_file> <lookback_minutes>')
+    print('Usage Example: $ python3 get_streamsets_job_metrics.py /tmp/streamsets_job_metrics.json 60')
+    sys.exit(1)
+
+
+# Check the number of command line args
+if len(sys.argv) != 3:
+    print('Error: Wrong number of arguments')
+    print_usage_and_exit()
+
+# Validate the job_metrics_file command line arg
+job_metrics_file = sys.argv[1]
+try:
+    output_file = open(job_metrics_file, "w", encoding='utf-8')
+except Exception as e:
+    print("Error: Could not create file: {}".format(job_metrics_file))
+    print(str(e))
+    sys.exit(-1)
+
+# Validate the lookback_minutes command line arg
+lookback_minutes = sys.argv[2]
+try:
+    lookback_minutes = int(lookback_minutes)
+except ValueError as ve:
+    print('Error: lookback_minutes arg \'{}\' is not an integer'.format(lookback_minutes))
+    print_usage_and_exit()
+
+
+def convert_timestamp_seconds_to_datetime_string(timestamp_seconds):
+    return datetime.fromtimestamp(timestamp_seconds).strftime("%Y-%m-%d %H:%M:%S")
+
+
+# Current time
+current_time_seconds = time()
+
+# Starting time to look for Jobs
+start_time_seconds = int(current_time_seconds - (lookback_minutes * 60))
+start_time_millis = start_time_seconds * 1000
+
+# Print the settings
+print('-------------------------------------')
+print('Current time is {}'
+      .format(convert_timestamp_seconds_to_datetime_string(current_time_seconds)))
+print('Lookback minutes is {}'.format(lookback_minutes))
+print('Will get metrics for Jobs started after {}'
+      .format(convert_timestamp_seconds_to_datetime_string(start_time_seconds)))
+print('Metrics will be written to the file {}'.format(job_metrics_file))
+print('-------------------------------------')
+
+# Connect to Control Hub
+sch = None
+try:
+    sch = ControlHub(
+        credential_id=cred_id,
+        token=cred_token)
+except Exception as e:
+    print('Error connecting to Control Hub')
+    print(str(e))
+    sys.exit(1)
+print('Connected to Control Hub')
+print('-------------------------------------')
+# Job runs to get metrics for
+job_runs = []
+
+
+# Get Job runs that were started after the lookback time
+def get_run_metrics(job_name, run_count, the_metrics):
+    for m in the_metrics:
+        if m.run_count == job_run.run_count:
+            return m
+    print('Error finding metrics for run #{} for Job {}'.format(run_count, job_name))
+    return None
+
+
+for job in sch.jobs:
+    # No need to look at Job Templates
+    if not job.job_template:
+
+        # Get the Job History
+        history = job.history
+
+        # Get the Job Metrics
+        metrics = job.metrics
+
+        done = False
+
+        # Loop through every Job Run, starting with the most recent
+        for job_run in history:
+
+            # If this Job Run was started or ended within the lookback period or is still ACTIVE
+            if (job_run.start_time >= start_time_millis
+                    or job_run.finish_time >= start_time_millis
+                    or job_run.status == 'ACTIVE'):
+
+                # Get the Job run's metrics
+                run = {}
+                run['ID'] = job.job_id
+                run['NAME'] = job.job_name
+                run['CREATETIME'] = job.created_on
+                run['LASTMODIFIEDON'] = job.last_modified_on
+                run['PIPELINENAME'] = job.pipeline_name
+                run['PIPELINECOMMITLABEL'] = job.commit_label
+                run['RUNCOUNT'] = job_run.run_count
+                run['STARTTIME'] = job_run.start_time
+                run['FINISHTIME'] = job_run.finish_time
+                run['ERRORMESSAGE'] = job_run.error_message
+                run['COLOR'] = job_run.color
+                run['STATUS'] = job_run.status
+                run_metrics = get_run_metrics(job.job_name, job_run.run_count, metrics)
+
+                # If no metrics exists, set all row counts to -1 as a flag
+                if run_metrics is not None:
+                    run['INPUTRECORDS'] = run_metrics.input_count
+                    run['OUTPUTRECORDS'] = run_metrics.output_count
+                    run['ERRORRECORDS'] = run_metrics.error_count
+                else:
+                    run['INPUTRECORDS'] = -1
+                    run['OUTPUTRECORDS'] = -1
+                    run['ERRORRECORDS'] = -1
+
+                job_runs.append(run)
+            else:
+                # We're finished with this Job
+                break
+
+print('Found {} Job Runs within lookback window'.format(len(job_runs)))
+
+if len(job_runs) > 0:
+    print('Writing Metrics')
+    try:
+        for run in job_runs:
+            output_file.write(json.dumps(run) + '\n')
+    finally:
+        output_file.close()
+
+print('-------------------------------------')
+print('Done')
